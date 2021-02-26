de3cf7aa172ba54e58a9d590c1e913c9eea94913
hzhu <zgjszhuhui@163.com>
2021-02-26 09:31:24 +0800

    nio buffer

--- src/main/java/com/fh/nio/BasicBuffer.java
+++ src/main/java/com/fh/nio/BasicBuffer.java
@@ -1,4 +1,22 @@
 package com.fh.nio;
 
+import java.nio.IntBuffer;
+
 public class BasicBuffer {
+    public static void main(String[] args) {
+        // 创建buffer, 大小为5
+        IntBuffer buffer = IntBuffer.allocate(10);
+        // 向buffer存放数据
+        for (int i = 0; i < 10; i++) {
+            buffer.put(i * 2);
+        }
+        // 从buffer读取数据
+
+        // 将buffer转换，转为读取
+        buffer.flip();
+        while (buffer.hasRemaining()) {
+            int number = buffer.get();
+            System.out.println(String.format("读取数据:%d", number));
+        }
+    }
 }
