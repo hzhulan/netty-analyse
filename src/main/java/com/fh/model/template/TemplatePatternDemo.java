1cea2a335b993c86fa588d0f462a1bd1a9f80309
hzhu <zgjszhuhui@163.com>
2021-03-01 17:49:36 +0800

    模板模式

--- src/main/java/com/fh/model/template/TemplatePatternDemo.java
+++ src/main/java/com/fh/model/template/TemplatePatternDemo.java
@@ -3,7 +3,7 @@ package com.fh.model.template;
 public class TemplatePatternDemo {
 
     /**
-     * 模板模式 简而言之就是 多态
+     * 模板模式
      * @param args
      */
     public static void main(String[] args) {
